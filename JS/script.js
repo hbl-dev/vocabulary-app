26ef29bb72035bb58ef2b68a18f2d7e87e1d7dad
hbl-dev <hlattauer@t-online.de>
2024-03-20 16:59:55 +0100

    adjustments

--- JS/script.js
+++ JS/script.js
@@ -22,7 +22,7 @@ function nextVocabulary() {
   let objectKeys = Object.keys(dictionary);
   randomEnglishWord = objectKeys[Math.floor(Math.random() * objectKeys.length)];
   if (dictionary[randomEnglishWord] === undefined) {
-    word.innerHTML = `No Vocabulary – Go add words.`;
+    word.innerHTML = `No words yet – go add some.`;
   } else {
     word.innerHTML = `${dictionary[randomEnglishWord]}`;
   }
